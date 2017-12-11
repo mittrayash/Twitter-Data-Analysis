dcdc05c65ca00e8bc7f7cfb1266b08fb1b03fdfb
mittrayash <mittrayash@gmail.com>
2017-12-11 13:51:09 +0530

    Minor changes in html, organized content, added screenshot

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ __author__ = 'mittr'
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from flask import Flask, render_template
-import pymongo
 
 
-#MONGODB_URI = 'mongodb://localhost:27017/delhi_mumbai'
+# MONGODB_URI = 'mongodb://localhost:27017/delhi_mumbai'
 MONGODB_URI = 'mongodb://user:pass@ds133856.mlab.com:33856/delhi_mumbai'
 # DB_URI Changed to remote one after uploading the files there
 client = MongoClient(MONGODB_URI)
@@ -53,18 +52,6 @@ def main():
     for x in hash_count.find():
         hashtag_count.append({"index":i, "hashtag":x["hashtag"], "count":x["count"]})
         i += 1
-
-    for x in all_tweets.find():  # get reply, mentions, and retweet edges
-        nodes.append((x["user_id"]))
-
-        if x['inReplyTo'] is not None:
-            reply_edges.append({"user_id": x["user_id"], "inReplyTo": x["inReplyTo"]})
-
-        if len(x['mentions']) > 0:
-            mntn = [x['mentions'][i] for i in range(len(x['mentions']))]
-            mention_edges.append({"user_id": x["user_id"], "mentions": mntn})
-        if x['Retweet']:
-            retweet_edges.append({"retweeter": x["user_id"], "retweeted": x["retweetedFrom"]})
 
     return render_template('index.html', outliers_count=outliers_count, location_count=location_count, nodes=nodes, reply_edges=reply_edges, mention_edges=mention_edges, retweet_edges=retweet_edges, orig_retweets=orig_retweets, hashtag_count=hashtag_count, media_count=media_count, favorite_count=favorite_count)
 
--- test.py
+++ test.py
@@ -1,10 +1,39 @@
 __author__ = 'mittr'
 import pymongo
 
+
 client = pymongo.MongoClient()
 db = client.delhi_mumbai
 coll = db.all_tweets
 nodes = []
+reply_edges = []
+mention_edges = []
+retweet_edges = []
+
 for x in coll.find():  # get reply, mentions, and retweet edges
-    nodes.append({"user_id": x["user_id"]})
-print(nodes)
+    nodes.append({"name": x["user"],"user_id": x["user_id"]})
+
+    if x['inReplyTo'] is not None:
+        reply_edges.append({"user_id": x["user_id"], "inReplyTo": x["inReplyTo"]})
+
+    if len(x['mentions']) > 0:
+        mntn = [x['mentions'][i] for i in range(len(x['mentions']))]
+        mention_edges.append({"user_id": x["user_id"], "mentions": mntn})
+    if x['Retweet']:
+        retweet_edges.append({'retweeter': x["user_id"], "retweeted": x["retweetedFrom"]})
+
+file = open('data.json', 'a', encoding="utf-8")
+file.write('{"nodes":[')
+for i in range(len(nodes)):
+    file.write((str(nodes[i])))
+    if i < len(nodes)-1:
+        file.write(',')
+    else:
+        file.write('],"links":[')
+
+for i in range(len(retweet_edges)):
+    file.write(str(retweet_edges[i]))
+    if i < len(retweet_edges)-1:
+        file.write(',')
+    else:
+        file.write(']}')
